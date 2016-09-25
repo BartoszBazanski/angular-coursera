e6b469abfc9251443a96ee20b30c9d12fcb4191b
Bartosz Bazański <bazanski.bartosz@gmail.com>
2016-09-25 23:17:16 +0200

    Create controllers and service for shopping list checkoff app

---
+++ Module2CodingAssignment/app.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict'
+  angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyShoppingController', ToBuyShoppingController)
+    .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    var toBuy = this;
+    toBuy.itemList = ShoppingListCheckOffService.getItemsToBuy();
+    toBuy.itemBought = function(index) {
+      ShoppingListCheckOffService.buyItem(index);
+    };
+  };
+
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    var bought = this;
+    bought.itemList = ShoppingListCheckOffService.getBoughtItems();
+  };
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+    var shoppingList = [
+      {name: "Cookies", quantity: 10},
+      {name: "Milk", quantity: 2},
+      {name: "Bread", quantity: 1},
+      {name: "Coffie", quantity: 3}
+    ];
+    var toBuyList = [];
+    var boughtItemList = [];
+    angular.forEach(shoppingList, function(item) {
+      toBuyList.push(item);
+    })
+    service.getItemsToBuy = function() {
+      return toBuyList;
+    };
+    service.buyItem = function(index) {
+      var boughtItem = toBuyList[index];
+      console.log(boughtItem);
+      console.log(boughtItemList);
+      toBuyList.splice(index, 1);
+      boughtItemList.push(boughtItem);
+      console.log(boughtItemList);
+    };
+    service.getBoughtItems = function() {
+      return boughtItemList;
+    };
+  };
+})();
